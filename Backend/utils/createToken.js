72067d6995d69088b8dd8ea18b0f0dcb919620ee
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-04-14 12:41:30 +0530

    updated

--- Backend/utils/createToken.js
+++ Backend/utils/createToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+    secure: process.env.NODE_ENV === 'development', // Use secure cookies in production
     sameSite: 'Strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
   });

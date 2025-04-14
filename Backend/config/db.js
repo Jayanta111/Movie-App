ad3749971c583b2a18f856383bde6fabf45992e8
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-04-14 08:23:51 +0530

    updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config(); // Load environment variables from .env
+dotenv.config(); 
 
 const connectDB = async () => {
   try {
